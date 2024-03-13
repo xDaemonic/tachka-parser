1beef122a1c026d6929073ef74028e5850bae37e
xDaemonic <errewer123@gmail.com>
2024-03-13 22:06:27 +0300

    dev 1.3

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def scrap_links(link: dict):
     f.close()
 
 if __name__ == '__main__':
-  categories_links = categories_worker.load_categories_links()
+  categories_links = categories_worker.get_categories_links()
   exit(200)
   for i in range(0, len(categories_links)):
     if categories_links[i]['url'] == 'https://tachka.ru/tormoza/tormozniye-shlangi':
