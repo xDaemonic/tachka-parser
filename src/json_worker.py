040df3b5843fc51d40a0085801bf904d59b1bc29
xDaemonic <errewer123@gmail.com>
2024-03-13 22:45:58 +0300

    dev 1.3

--- src/json_worker.py
+++ src/json_worker.py
@@ -2,7 +2,7 @@ import json
 from src import categories_worker
 
 def set_process_category_status(url: str, status: bool) -> None:
-  with open(categories_worker.get_category_filepath(), 'a+') as file:
+  with open(categories_worker.get_category_filepath(), 'w+') as file:
     items = json.load(file)
     for i in range(0, len(items)):
       if (items[i]['url'] == url):
