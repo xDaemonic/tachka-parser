1b5b417d841624fb7cd5c8b7f1a637a8595bab8f
xDaemonic <errewer123@gmail.com>
2024-03-13 22:25:18 +0300

    dev 1.3

--- main.py
+++ main.py
@@ -28,11 +28,13 @@ def scrap_links(link: dict):
     f.close()
 
 if __name__ == '__main__':
-  with open(categories_worker.get_category_filepath(), 'a+') as file:
-    file.truncate(0)
-    file.close()
+  # with open(categories_worker.get_category_filepath(), 'a+') as file:
+  #   file.truncate(0)
+  #   file.close()
   
-  categories_links = categories_worker.load_categories_links()
+  categories_links = categories_worker.get_categories_links()
+  print(categories_links)
+  exit(200)
   # categories_links = list(filter(lambda item: not item['proce'], categories_links))
   
   # print(categories_links[0])
