b842b366ec82e5207f072d7e07225a4b12ef38de
xDaemonic <errewer123@gmail.com>
2024-03-13 13:18:21 +0300

    dev 1.3

--- main.py
+++ main.py
@@ -3,25 +3,35 @@ from src import pages_worker
 from src import format
 from src import helpers
 
+from multiprocessing import Pool
+
 import requests, json
 
+def scrap_links(link: dict):
+  category = format.remove_base_url(link['url'])
+  resp = requests.get(link['url'], proxies=proxies)
+  page = 0
+  result = []
+  while resp.status_code == 200:
+    print(f"category: {category}, page: {page}\r\n")
+    links = pages_worker.process_category_page(resp.text)
+    result = helpers.merge(result, links)
+    
+    page += 1
+    path = format.page_url(link['url'], page)
+    resp = requests.get(path, proxies=proxies)
+          
+  with open(f"./json/{category}.json", 'a+') as file:
+    json.dump(result, file)
+    file.close()
+
+
 if __name__ == '__main__':
-  categories_links = categories_worker.get_categories_links()
+  categories_links = categories_worker.load_categories_links()
+  proxies = {
+    'https': 'https://185.225.232.191:80'
+  }
+  pool = Pool()
   for link in categories_links:
-    category = format.remove_base_url(link['url'])
-    resp = requests.get(link['url'])
-    page = 0
-    result = []
-    while resp.status_code == 200:
-      print(f"category: {category}, page: {page}\r\n")
-      links = pages_worker.process_category_page(resp.text)
-      result = helpers.merge(result, links)
-      
-      page += 1
-      path = format.page_url(link['url'], page)
-      resp = requests.get(path)
-      
-    with open(f"./json/{category}.json", 'a+') as file:
-      json.dump(result, file)
-      file.close()
+    pool.apply_async(scrap_links, link)
     
