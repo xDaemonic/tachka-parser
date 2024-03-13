68fa912fc7ba34258546e31f68b4869f690389f2
xDaemonic <errewer123@gmail.com>
2024-03-13 14:32:26 +0300

    dev 1.3

--- main.py
+++ main.py
@@ -36,11 +36,11 @@ if __name__ == '__main__':
   for link in categories_links:
     print(link)
     p = mp.Process(target=scrap_links, args=(link,))
-    p.start()
-    p.join()
-    # process_list.append(p)
+    # p.start()
+    # p.join()
+    process_list.append(p)
     
     
-  # map(lambda process: process.start(), process_list)
-  # map(lambda process: process.join(), process_list)
+  map(lambda process: process.start(), process_list)
+  map(lambda process: process.join(), process_list)
     
