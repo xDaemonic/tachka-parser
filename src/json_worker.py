e51dac8fca527ab575b6d364a04106dd5e8a8b14
xDaemonic <errewer123@gmail.com>
2024-03-14 12:19:27 +0300

    dev 1.3

--- src/json_worker.py
+++ src/json_worker.py
@@ -3,7 +3,7 @@ from src import categories_worker, helpers
 
 
 def get_product_links():
-  files = list(map(lambda elem: elem != './json/categories_links.json', glob.glob('./json/*.json')))
+  files = list(filter(lambda elem: elem != './json/categories_links.json', glob.glob('./json/*.json')))
   print(files)
   print(len(files))
   
