b4d515796d03057acbf7e4cc52f99b876965b0a5
xDaemonic <errewer123@gmail.com>
2024-03-14 11:26:12 +0300

    dev 1.3

--- src/tests.py
+++ src/tests.py
@@ -22,7 +22,7 @@ def databse_connection():
   connection = db.get_connection()
   cursor = connection.cursor()
   cursor.execute('''
-                 SELECT 
+                  SELECT 
                       name
                   FROM 
                       sqlite_schema
@@ -34,9 +34,19 @@ def databse_connection():
   result = cursor.fetchall()
   return [table[0] for table in result]
 
-def database_table_coluns(table: str):
+def database_table_coluns(table: str) ->list:
   connection = db.get_connection()
   cursor = connection.cursor()
   cursor.execute(f"SELECT * FROM {table} LIMIT 1")
   
   return [description[0] for description in cursor.description]
+
+
+def run():
+  fails = json_files_valid()
+  tables = databse_connection()
+  cols = []
+  for table in tables:
+    cols.append(database_table_coluns(table).append(table))
+  
+  print(fails, table, cols)
