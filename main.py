8a1e9504421f4716261d853ddfd45d557113ec91
xDaemonic <errewer123@gmail.com>
2024-03-14 11:53:35 +0300

    dev 1.3

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from src import json_worker
 from src import tests
 from src import migrations
 from src import db
+from src import scraper
 
 import multiprocessing as mp
 import sqlite3
@@ -13,7 +14,8 @@ import requests, json, os, glob
 
 if __name__ == '__main__':
   links = db.get_unprocessed_categories_links()
-  
+  for link in links:
+    scraper.catch_links(link)
   # con = db.get_connection()
   # cur = con.cursor()
   
