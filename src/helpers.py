a6c89bc2efa315f3ad863da0e79c54a184258744
xDaemonic <errewer123@gmail.com>
2024-03-15 18:09:59 +0300

    dev 1.3

--- src/helpers.py
+++ src/helpers.py
@@ -1,9 +1,6 @@
 def merge(list1: list, list2: list) -> list:
   return [x for n in (list1, list2) for x in n]
 
-def chunker(l, s):
+def chunks(l, s):
   for i in range(0, len(l), s):
     yield l[i:i + s]
-
-def chunks(lst: list, n: int):
-  return list(chunker(lst, n))
