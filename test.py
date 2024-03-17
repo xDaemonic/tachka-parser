3bec3f7041ad7c33ca09c2cfd03986150a788ac1
xDaemonic <errewer123@gmail.com>
2024-03-17 09:41:17 +0300

    dev 1.3

--- test.py
+++ test.py
@@ -3,4 +3,13 @@ from src import tests, db
 conn = db.get_connection()
 cur = conn.cursor()
 cur.execute('SELECT COUNT(*) FROM product_links WHERE proc = 0')
+print("unproc links count")
+print(cur.fetchone())
+
+cur.execute('SELECT COUNT(*) FROM products')
+print('products count')
+print(cur.fetchone())
+
+cur.execute('SELECT COUNT(*) FROM product_links')
+print('total poroduct links')
 print(cur.fetchone())
